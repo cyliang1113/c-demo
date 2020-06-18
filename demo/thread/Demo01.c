59c889a92a1f63dd9b2b65d5889a5cfa805a6851
leo <cyliang1113@126.com>
2020-06-18 20:34:13 +0800

    xxxxxxxxxx

--- demo/thread/Demo01.c
+++ demo/thread/Demo01.c
@@ -3,35 +3,42 @@
 //
 
 #include "Demo01.h"
-#include <stdio.h>
-//#include <stdlib.h>
 
 #include <pthread.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
 void *run(void *args) {
-    printf("run... \n");
-//    for (int i = 0; i < 5; i++) {
-//        *(int *) args = *(int *) args + 1;
-//        printf("thread count: %d\n", *(int *) args);
-//    }
+    printf("run...\n");
+    for (int i = 0; i < 50; i++) {
+        *(int *) args = (*(int *) args) + 1;
+        printf("run count: %d\n", *(int *) args);
+    }
+}
+
+void *run2(void *args) {
+    printf("run2...\n");
+    for (int i = 0; i < 50; i++) {
+        *(int *) args = (*(int *) args) + 1;
+        printf("run2 count: %d\n", *(int *) args);
+    }
 }
 
 void create_thread01(){
 //    printf("1111111111\n");
-    pthread_t *thread;
-
+    pthread_t thread;
+    pthread_t thread2;
 //    printf("22222222222\n");
-//    void *count = malloc(sizeof(int));
-//    *count = 1;
+    int *count = malloc(sizeof(int));
+    *count = 0;
 
-//    printf("33333333333\n");
-    int r = pthread_create(thread, NULL, run, NULL);
+    int r = pthread_create(&thread, NULL, run, (void *)count);
     printf("pthread_create r: %d\n", r);
-
-//    for (int j = 0; j < 5; j++) {
-//        *count = *count + 1;
-//        printf("main count: %d\n", *count);
-//    }
+//    int r2 = pthread_create(&thread2, NULL, run2, (void *)count);
+//    printf("pthread_create r2: %d\n", r2);
+    pthread_join(thread, &r);
 }
 
 
